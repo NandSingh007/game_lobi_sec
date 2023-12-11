b105bc2a92cdea6a630c84809d6b5161106a7117
shubham singh Rathore <138260347+NandSingh007@users.noreply.github.com>
2023-12-11 23:34:24 +0530

    Updates

--- src/component/ReactRedux/Store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { dataReducer } from '../ReactRedux/Reducer'
-const store = configureStore({
-    reducer: {
-        add: dataReducer,
-    }
-});
-
-export default store
